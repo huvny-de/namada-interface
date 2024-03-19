ce3938c385106d8d3ec14db8c63563cb0dc4bbf5
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2024-03-19 12:53:15 +0100

    chore: cleanup path utils

--- packages/utils/src/helpers/index.ts
+++ packages/utils/src/helpers/index.ts
@@ -1,6 +1,5 @@
 import { JsonRpcRequest } from "@cosmjs/json-rpc";
 import {
-  Bip44Path,
   JsonCompatibleArray,
   JsonCompatibleDictionary,
   Tokens,
@@ -177,36 +176,6 @@ export const Result = {
   err: function <E>(error: E): Err<E> {
     return { ok: false as const, error };
   },
-};
-
-/**
- * Return a properly formatted BIP-044 path
- *
- * @param {number} coinType - SLIP-044 Coin designation
- * @param {Bip44Path} bip44Path - path object
- * @returns {string}
- */
-export const makeBip44Path = (
-  coinType: number,
-  bip44Path: Bip44Path
-): string => {
-  const { account, change, index } = bip44Path;
-  return `m/44'/${coinType}'/${account}'/${change}'/${index}'`;
-};
-
-/**
- * Return a properly formatted BIP-044 path array
- *
- * @param {number} coinType - SLIP-044 Coin designation
- * @param {Bip44Path} path - path object
- * @returns {string}
- */
-export const makeBip44PathArray = (
-  coinType: number,
-  path: Bip44Path
-): Uint32Array => {
-  const { account, change, index } = path;
-  return new Uint32Array([44, coinType, account, change, index]);
 };
 
 /**
